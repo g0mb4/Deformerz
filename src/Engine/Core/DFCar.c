189b71b5c439da459d51c3d62f5b1667dc1ff4d8
Tóth János <gomba007@gmail.com>
2026-08-25 12:44:49 +0200

    Make it a bit more stable.

--- src/Engine/Core/DFCar.c
+++ src/Engine/Core/DFCar.c
@@ -25,7 +25,7 @@ void init_springs(DFCar* dfcar)
         Vector3 pA = ToVector3(points[seg->pointA].pos);
         Vector3 pB = ToVector3(points[seg->pointB].pos);
         dfcar->springs[i].restLegnth = Vector3Distance(pA, pB); // TODO(gmb): from .DAT
-        dfcar->springs[i].stiffness = 100.0f;                   // TODO(gmb): from .DAT
+        dfcar->springs[i].stiffness = 1500.0f;                  // TODO(gmb): from .DAT
     }
 }
 
@@ -72,16 +72,15 @@ void DFCar_Update()
 
     // apply gravity
     for (size_t i = 0; i < car->pointCount; i++) {
-        //forces[i].y += -GRAVITY;
+        forces[i].y += -GRAVITY;
     }
 
-    // Hookes law
+    // Hooke's law
     // TODO(gmb): damping ?
-    TerepCarPoint* points = car->points;
     for (size_t i = 0; i < car->physSegmentCount; i++) {
         TerepCarPhysSegment* seg = &car->physSegments[i];
-        Vector3 pA = ToVector3(points[seg->pointA].pos);
-        Vector3 pB = ToVector3(points[seg->pointB].pos);
+        Vector3 pA = ToVector3(car->points[seg->pointA].pos);
+        Vector3 pB = ToVector3(car->points[seg->pointB].pos);
 
         float currentLength = Vector3Distance(pA, pB);
         float restLength = dfcar->springs[i].restLegnth;
@@ -91,13 +90,38 @@ void DFCar_Update()
         forces[seg->pointB] = Vector3Subtract(forces[seg->pointB], springForce);
     }
 
+    // resolve collisions with the map
+    for (size_t i = 0; i < car->pointCount; i++) {
+        TerepCarPoint* point = &car->points[i];
+        if (point->type == TEREP_POINT_GEOMETRY) {
+            // TODO(gmb): resolve point collision here
+        } else if (point->type == TEREP_POINT_WHEEL_FRONT || point->type == TEREP_POINT_WHEEL_REAR) {
+            // TODO(gmb): resolve shpere collision here
+        } else {
+            // DO NOTHING
+        }
+    }
+
     // NOTE(gmb): using Explicit-Euler integration for now, but we can switch to Verlet if needed
     for (size_t i = 0; i < car->pointCount; i++) {
+        // NOTE(gmb): mass is assumed to be 1
         Engine.car->vel[i] = Vector3Add(dfcar->vel[i], Vector3Scale(forces[i], dt));
         Vector3 pos = Vector3Add(ToVector3(car->points[i].pos), Vector3Scale(dfcar->vel[i], dt));
 
         car->points[i].pos[0] = pos.x;
         car->points[i].pos[1] = pos.y;
         car->points[i].pos[2] = pos.z;
+    }
+
+    // clamp wheels
+    for (size_t i = 0; i < car->pointCount; i++) {
+        TerepCarPoint* point = &car->points[i];
+        if (point->type == TEREP_POINT_WHEEL_FRONT || point->type == TEREP_POINT_WHEEL_REAR) {
+            Vector3 pos = ToVector3(point->pos);
+            float mapHeight = -0.1; // TODO(gmb): get it from heightmap
+            if (pos.y < mapHeight) {
+                point->pos[1] = mapHeight;
+            }
+        }
     }
 }
